7b2c8f629d411a771d8869bca9401b5acff13290
Kenneth Doerflein <ktd500@verizon.net>
2024-04-03 19:14:05 -0400

    update settings.py directory paths

--- JSG/JSG/settings.py
+++ JSG/JSG/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,11 +50,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'JSG.urls'
-
+dir_path = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["../JSG/templates"],
+        "DIRS": [dir_path,],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ["../JSG/static"]
+static_dir_path = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [static_dir_path,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
