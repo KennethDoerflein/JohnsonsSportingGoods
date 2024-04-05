e7c4b65762c967a2d609186d41e1159649047796
Kenneth Doerflein <ktd500@verizon.net>
2024-04-05 18:01:48 -0400

    Add 'Product' model

--- JSG/store/models.py
+++ JSG/store/models.py
@@ -1,3 +1,20 @@
+import os
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+def images_path():
+    # this may not be right
+    return os.path.join(settings.LOCAL_FILE_DIR, "/static/assets/")
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    image = models.FilePathField(
+        path=images_path,
+        default="/static/assets/johnsonLogo.png",
+    )
