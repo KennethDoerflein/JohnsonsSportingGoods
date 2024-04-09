9f6bd8a9a66624580a27440c6c7e69f05ab4f2c8
Kenneth Doerflein <ktd500@verizon.net>
2024-04-08 23:57:24 -0400

    cleanup

--- JSG/store/views.py
+++ JSG/store/views.py
@@ -1,43 +1,28 @@
-import re
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Product
 from .forms import RegistrationForm
 from django.contrib.auth.models import Group
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 
 
 def index(request):
     products = Product.objects.all().values()
-    # template = loader.get_template("index.html")
-    # return HttpResponse(template.render(request, {"products": products}))
     return render(request, "index.html", {"products": products})
 
 
 def cart(request):
-    # template = loader.get_template("cart.html")
-    # return HttpResponse(request, template.render())
     return render(request, "cart.html")
 
 
 def orderConfirmation(request):
-    # template = loader.get_template("orderConfirmation.html")
-    # return HttpResponse(request, template.render())
     return render(request, "orderConfirmation.html")
 
 
 def login(request):
     if request.method == "POST":
-        #     username = request.POST["username"]
-        #     password = request.POST["password"]
-        #     user = authenticate(request, username=username, password=password)
-        #     if user is not None:
-        #         auth_login(request, user)
-        #         return redirect("index")
-        # form = {}
-        # form["form"] = LoginForm()
         copyRequest = request.POST.copy()
         copyRequest["username"] = copyRequest["username"].lower()
         form = AuthenticationForm(data=copyRequest)
@@ -45,7 +30,6 @@ def login(request):
             formUsername = form.cleaned_data.get("username")
             formPassword = form.cleaned_data.get("password")
             user = authenticate(request, username=formUsername, password=formPassword)
-            # user = form.get_user()
             if user is not None:
                 auth_login(request, user)
                 return redirect("index")
