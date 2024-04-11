2471a7177d7ac0e6cb8c38068350a947b37e7572
Bruce Kirschbaum <brucejr241@gmail.com>
2024-04-10 22:40:21 -0400

    Add extra UI to index and ability to add to cart

--- JSG/store/views.py
+++ JSG/store/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-from .models import Product
+from .models import Product, Cart
+from django.db.models import Sum
 from .forms import RegistrationForm
 from django.contrib.auth.models import Group
 from django.contrib.auth.forms import AuthenticationForm
@@ -10,11 +11,39 @@ from django.contrib.auth import login as auth_login, logout as auth_logout, auth
 
 def index(request):
     products = Product.objects.all().values()
-    return render(request, "index.html", {"products": products})
+    cart_count = Cart.objects.filter(CID=request.user.id).aggregate(Sum('qty'))['qty__sum'] or 0
+
+    context = {
+        'products': products,
+        'cart_count': cart_count,
+    }
+
+    return render(request, "index.html", context)
 
 
 def cart(request):
-    return render(request, "cart.html")
+    cart_count = Cart.objects.filter(CID=request.user.id).aggregate(Sum('qty'))['qty__sum'] or 0
+    cart_items = Cart.objects.filter(CID=request.user.id)
+
+    cart_items_with_product_details = []
+    total_cost = 0
+    for cart_item in cart_items:
+        product = Product.objects.get(id=cart_item.PID)
+        total_cost += product.price * cart_item.qty
+        cart_item_with_product_details = {
+            'name': product.name,
+            'price': product.price * cart_item.qty,
+            'qty': cart_item.qty
+        }
+        cart_items_with_product_details.append(cart_item_with_product_details)
+
+    context = {
+        'cart_count': cart_count,
+        'cart_items': cart_items_with_product_details,
+        'total_cost': total_cost
+    }
+
+    return render(request, "cart.html", context)
 
 
 def orderConfirmation(request):
@@ -56,3 +85,13 @@ def logout(request):
     if request.method == "POST":
         auth_logout(request)
         return redirect("index")
+
+def add_to_cart(request):
+    if request.method == 'POST':
+        customer_id = request.POST.get('customer_id')
+        product_id = request.POST.get('product_id')
+        quantity = request.POST.get('quantity')
+
+        Cart.objects.create(CID=customer_id, PID=product_id, qty=quantity)
+
+        return redirect('index')
