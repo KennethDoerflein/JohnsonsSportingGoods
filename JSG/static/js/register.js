e72a82e9200b3f7bbdd11cc7402de0e4df46133b
Kenneth Doerflein <ktd500@verizon.net>
2024-04-12 23:48:50 -0400

    check username/email are used on register form

--- JSG/static/js/register.js
+++ JSG/static/js/register.js
@@ -1,14 +1,20 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
 
-document.getElementById("id_username").onkeyup = validateForm;
-document.getElementById("id_email").onkeyup = validateForm;
-document.getElementById("id_first_name").onkeyup = validateForm;
-document.getElementById("id_last_name").onkeyup = validateForm;
-document.getElementById("id_password1").onkeyup = validateForm;
-document.getElementById("id_password2").onkeyup = validateForm;
+var emailUnused = false;
+var usernameUnused = false;
 
-function validateForm(self) {
+document.getElementById("id_first_name").oninput = validateForm;
+document.getElementById("id_last_name").oninput = validateForm;
+
+document.getElementById("id_password1").oninput = checkPassword;
+document.getElementById("id_password2").oninput = checkPassword;
+
+document.getElementById("id_username").oninput = checkUsername;
+
+document.getElementById("id_email").oninput = checkEmail;
+
+function validateForm() {
   var username = document.getElementById("id_username").value;
   var email = document.getElementById("id_email");
   var fName = document.getElementById("id_first_name").value;
@@ -17,17 +23,123 @@ function validateForm(self) {
   var password2 = document.getElementById("id_password2");
 
   var toolTip = document.getElementById("ToolTipRegister");
-  var loginButton = document.getElementById("registerButton");
+  var registerButton = document.getElementById("registerButton");
 
   var emailRegex = RegExp(/^\w+([\.-]?(?=(\w+))\1)*@\w+([\.-]?(?=(\w+))\1)*(\.\w{2,3})+$/).test(email.value);
 
-  if (username !== "" && email.value !== "" && emailRegex && fName !== "" && lName !== "" && password1.value !== "" && password2.value !== "" && password1.value === password2.value) {
+  console.log(emailRegex);
+
+  console.log(emailUnused);
+
+  console.log(usernameUnused);
+
+  if (
+    username !== "" &&
+    email.value !== "" &&
+    emailRegex &&
+    fName !== "" &&
+    lName !== "" &&
+    password1.value !== "" &&
+    password2.value !== "" &&
+    password1.value === password2.value &&
+    emailUnused &&
+    usernameUnused
+  ) {
     bootstrap.Tooltip.getInstance(toolTip).disable();
-    loginButton.removeAttribute("disabled");
+    registerButton.removeAttribute("disabled");
   } else {
     bootstrap.Tooltip.getInstance(toolTip).enable();
-    loginButton.setAttribute("disabled", "");
+    registerButton.setAttribute("disabled", "");
   }
+}
+
+function checkUsername(self) {
+  var username = document.getElementById("id_username");
+  var usernameHelpText = document.getElementById("id_username_helptext");
+  fetch("checkTaken?username=" + self.target.value)
+    .then((response) => response.text())
+    .then((text) => {
+      var status = JSON.parse(text)["used"];
+      if (self.target.value !== "") {
+        if (status === "true") {
+          username.classList.add("inputERR");
+          username.classList.remove("inputGood");
+          usernameUnused = false;
+          usernameHelpText.classList.add("helptextBad");
+          usernameHelpText.classList.remove("helptext");
+          usernameHelpText.innerText = "That username is already in use";
+        } else {
+          usernameUnused = true;
+          username.classList.add("inputGood");
+          username.classList.remove("inputERR");
+          usernameHelpText.innerText = "That looks good";
+          usernameHelpText.classList.remove("helptextBad");
+          usernameHelpText.classList.add("helptext");
+        }
+      } else {
+        username.classList.remove("inputERR");
+        username.classList.remove("inputGood");
+        usernameHelpText.classList.remove("helptextBad");
+        usernameHelpText.classList.add("helptext");
+        usernameHelpText.innerText = "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.";
+        usernameUnused = false;
+      }
+      validateForm();
+    });
+  validateForm();
+}
+
+function checkEmail(self) {
+  var email = document.getElementById("id_email");
+  var emailHelpText = document.getElementById("id_email_helptext");
+  var emailRegex = RegExp(/^\w+([\.-]?(?=(\w+))\1)*@\w+([\.-]?(?=(\w+))\1)*(\.\w{2,3})+$/).test(email.value);
+  if (!emailRegex) {
+    if (email.value === "") {
+      emailUnused = false;
+      email.classList.remove("inputGood");
+      email.classList.remove("inputERR");
+      emailHelpText.classList.remove("helptextBad");
+      emailHelpText.classList.add("helptext");
+      emailHelpText.innerText = "Required: Enter a email in the format example@example.com";
+    } else {
+      emailUnused = true;
+      email.classList.add("inputERR");
+      email.classList.remove("inputGood");
+      emailHelpText.classList.add("helptextBad");
+      emailHelpText.classList.remove("helptext");
+      emailHelpText.innerText = "Email is not in the correct format, ex: example@example.com";
+    }
+  } else {
+    fetch("checkTaken?email=" + self.target.value)
+      .then((response) => response.text())
+      .then((text) => {
+        var status = JSON.parse(text)["used"];
+        if (self.target.value !== "") {
+          if (status === "true") {
+            email.classList.add("inputERR");
+            email.classList.remove("inputGood");
+            emailHelpText.classList.add("helptextBad");
+            emailHelpText.classList.remove("helptext");
+            emailUnused = false;
+            emailHelpText.innerText = "That email is already in use";
+          } else if (emailRegex) {
+            email.classList.add("inputGood");
+            email.classList.remove("inputERR");
+            emailUnused = true;
+            emailHelpText.classList.remove("helptextBad");
+            emailHelpText.classList.add("helptext");
+            emailHelpText.innerText = "That looks good";
+          }
+        }
+        validateForm();
+      });
+  }
+  validateForm();
+}
+
+function checkPassword(self) {
+  var password1 = document.getElementById("id_password1");
+  var password2 = document.getElementById("id_password2");
   if (password1.value !== "" && password2.value !== "") {
     if (password1.value !== password2.value) {
       password1.classList.add("inputERR");
@@ -46,15 +158,5 @@ function validateForm(self) {
       password2.classList.remove("inputERR");
     }
   }
-
-  if (email.value !== "") {
-    if (!emailRegex) {
-      email.classList.add("inputERR");
-      email.setCustomValidity("Email is not formatted correctly.");
-      email.reportValidity();
-    } else {
-      email.setCustomValidity("");
-      email.classList.remove("inputERR");
-    }
-  }
+  validateForm();
 }
--- JSG/store/views.py
+++ JSG/store/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Product
 from .forms import RegistrationForm, LoginForm
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.http import JsonResponse
 
 
 def index(request):
@@ -54,3 +55,16 @@ def logout(request):
     if request.method == "POST":
         auth_logout(request)
         return redirect("index")
+
+
+def checkTaken(request):
+    username = request.GET.get("username")
+    email = request.GET.get("email")
+    if username is not None:
+        if User.objects.filter(username=username.lower()).exists():
+            return JsonResponse({"used": "true"})
+        return JsonResponse({"used": "false"})
+    elif email is not None:
+        if User.objects.filter(email=email.lower()).exists():
+            return JsonResponse({"used": "true"})
+        return JsonResponse({"used": "false"})
