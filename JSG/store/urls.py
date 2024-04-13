0234f437fbb7638bdce7ad6a42c8e47710bbc1c1
Kenneth Doerflein <ktd500@verizon.net>
2024-04-12 23:47:09 -0400

    check username/email are used on register form

--- JSG/store/urls.py
+++ JSG/store/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("register", views.register, name="register"),
     path("logout", views.logout, name="logout"),
+    path("checkTaken", views.checkTaken, name="checkTaken"),
 ]
