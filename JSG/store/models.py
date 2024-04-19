50a50939088de69b598b157bcf7d68f8213f7948
Kenneth Doerflein <ktd500@verizon.net>
2024-04-19 13:12:31 -0400

    remove unneeded models, migration s

--- JSG/store/models.py
+++ JSG/store/models.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
-from datetime import date
 
 
 # Create your models here.
@@ -21,19 +19,3 @@ class Cart(models.Model):
     CID = models.BigIntegerField()
     PID = models.BigIntegerField()
     qty = models.IntegerField()
-
-class Order(models.Model):
-    order_id = models.CharField(max_length=10, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    shipping_address = models.CharField(max_length=100)
-    billing_address = models.CharField(max_length=100)
-    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
-    order_date = models.DateField(default=date.today)  
-    
-
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-
-    
